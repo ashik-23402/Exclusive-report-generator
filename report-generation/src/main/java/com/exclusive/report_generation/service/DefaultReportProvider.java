92e2cab5bd475535baae175487885cbfe28cca2b
Md Ashikur Rahman <102418282+ashik-23402@users.noreply.github.com>
2026-05-12 23:01:59 +0600

    Update DefaultReportProvider.java

--- report-generation/src/main/java/com/exclusive/report_generation/service/DefaultReportProvider.java
+++ report-generation/src/main/java/com/exclusive/report_generation/service/DefaultReportProvider.java
@@ -5,30 +5,4 @@ import com.exclusive.report_generation.model.Report;
 import com.exclusive.report_generation.utils.enums.ReportType;
 
 public class DefaultReportProvider {
-
-    private static DefaultReportProvider instance;
-
-    private final Report defaultReport;
-
-    private DefaultReportProvider() {
-
-        this.defaultReport = new Report(
-                AppConstants.DEFAULT_REPORT_NAME,
-                AppConstants.DEFAULT_GENERATED_BY,
-                ReportType.DAILY
-        );
-    }
-
-    public static synchronized DefaultReportProvider getInstance() {
-        if (instance == null) {
-            instance = new DefaultReportProvider();
-        }
-
-        return instance;
-    }
-
-    public Report getDefaultReport() {
-
-        return defaultReport;
-    }
 }
