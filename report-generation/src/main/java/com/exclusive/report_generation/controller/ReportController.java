0bebf1f2d593ea2f51570f3107aa9618de3ad889
Md Ashikur Rahman <102418282+ashik-23402@users.noreply.github.com>
2026-05-12 23:00:40 +0600

    Update ReportController.java

--- report-generation/src/main/java/com/exclusive/report_generation/controller/ReportController.java
+++ report-generation/src/main/java/com/exclusive/report_generation/controller/ReportController.java
@@ -7,19 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/v1/reports")
 public class ReportController {
-
-    private final ReportService reportService;
-
-    public ReportController(ReportService reportService) {
-        this.reportService = reportService;
-    }
 
     @GetMapping("/generate")
     public Report generateReport() throws InterruptedException {
 
-        return reportService.generateReport(ReportType.DEFAULT);
+        return "reportService.generateReport(ReportType.DEFAULT)";
     }
 }
